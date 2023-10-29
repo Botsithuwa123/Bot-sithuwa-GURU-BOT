7dcf6a59ff26c9bc039ec5ebaf7a68b23d80e4ab
Botsithuwa123 <149279895+Botsithuwa123@users.noreply.github.com>
2023-10-29 13:15:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94755975860', 'me', true],
+  ['94755975860', 'Bot sithum ', true], 
   [''] 
 ] //Number of owners
 
